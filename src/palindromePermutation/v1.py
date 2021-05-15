af16a2bf8da842810e96335d5c6f11478001d484
liampatterson02@gmail.com <liampatterson02@gmail.com>
2021-05-15 09:45:57 -0400

    changed dict index to use current character

--- src/palindromePermutation/v1.py
+++ src/palindromePermutation/v1.py
@@ -2,15 +2,21 @@ def isPalindromePermutation(string):
     dict = {}
     foundPivot = 0
     for i in range(len(string)):
-        if string[i].isalpha():
-            if( string[i] in dict and dict[i]):
-                dict[i] = 0
+        ch = string[i]
+        if ch.isalpha():
+            
+            if( ch in dict and dict[ch] == 1):
+                dict[ch] = 0
                 foundPivot -= 1
             else:
                 foundPivot += 1
-                dict[i] = 1
+                dict[ch] = 1
+    
     return foundPivot <= 1
 
 print(isPalindromePermutation("racecar"))
 print(isPalindromePermutation("acerrac"))
 print(isPalindromePermutation("liam"))
+print(isPalindromePermutation("racecar!-"))
+print(isPalindromePermutation("poo"))
+print(isPalindromePermutation("santnas"))
