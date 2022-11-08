b4bf35660cd6ae7cc0464d3b1f176e6bdee917e2
liampatterson02@gmail.com <liampatterson02@gmail.com>
2022-11-07 20:08:30 -0500

    added unique paths problem

---
+++ bin/Recursion-Dynamic/uniquePaths.py
@@ -0,0 +1,35 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        paths = {}
+    
+        def numFromPos(row, col):
+            
+            if (row == m -1 or col == n -1):
+                return 1
+
+            res = 0
+
+            if ((row + 1, col) in paths):
+                res += paths[(row+1, col)]
+            else:
+                numPaths = numFromPos(row+1, col)
+                res += numPaths
+                paths[(row+1, col)] = numPaths
+
+            if ((row, col+1) in paths):
+                res += paths[(row, col+1)]
+            else:
+                numPaths = numFromPos(row, col+1)
+                res += numPaths
+                paths[(row, col+1)] = numPaths
+
+            
+            return res
+        
+        return numFromPos(0,0)
+
+
+sol = Solution()
+
+print(sol.uniquePaths(3, 7))
